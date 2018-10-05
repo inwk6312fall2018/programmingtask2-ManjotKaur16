93e003ecdb74ee0ff1aa21b60bc70f92ec399255
Manjot <Manjot.Kaur@dal.ca>
2018-10-04 21:51:00 -0700

    creates task file

---
+++ task.py
@@ -0,0 +1,15 @@
+import pandas as pd    #panda module for read and write .CSV file
+counter_assault=0
+counter_robbery=0
+user=pd.read_csv("crime.csv")   
+a=user["RUCR_EXT_D"]
+for word in a:
+    if word == "ASSAULT":
+        counter_assault += 1
+    elif word == "ROBBERY":
+        counter_robbery += 1
+data={"Crime Type":["ASSAULT","ROBBERY"], "Crime ID":[1430,2142], "Crime Count":[counter_assault,counter_robbery]}
+new_CSV=pd.DataFrame(data,columns=["Crime Type","Crime ID","Crime Count"])
+print(new_CSV)
+new_CSV.to_csv("crime1.csv")
+
